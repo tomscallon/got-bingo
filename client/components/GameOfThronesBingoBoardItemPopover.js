53a32a817a89df2ce79201d4e5e9d60048c9ef7c
Tom Scallon <tssnowguy13@gmail.com>
2018-10-31 22:39:29 -0700

    Add character filtering in board item popover

--- client/components/GameOfThronesBingoBoardItemPopover.js
+++ client/components/GameOfThronesBingoBoardItemPopover.js
@@ -4,17 +4,44 @@ import styles from './GameOfThronesBingoBoardItemPopover.scss';
 
 import * as React from 'react';
 
-type Props = {};
+import characters from '../data/characters';
 
-class GameOfThronesBingoBoardItemPopover extends React.Component<Props> {
+type Props = {};
+type State = { search: string };
+
+class GameOfThronesBingoBoardItemPopover extends React.Component<Props, State> {
+  state = { search: '' };
+
+  onInputChange = (ev: Event) => this.setState({ search: ev.target.value });
+
+  renderCharacters(): React.Node {
+    const search = this.state.search.toLowerCase();
+    const matchingCharacters = search
+      ? characters.filter(
+          c =>
+            c.name.toLowerCase().includes(search) ||
+            c.nicknames.some(name => name.toLowerCase().includes(search)),
+        )
+      : characters;
+
+    return (
+      <div>
+        {matchingCharacters.map(c => (
+          <div>{c.name}</div>
+        ))}
+      </div>
+    );
+  }
+
   render(): React.Node {
     return (
       <div>
         <input
           className={styles.search}
           placeholder="Search for a character..."
+          onChange={this.onInputChange}
         />
-        <div>Characters will appear here...</div>
+        {this.renderCharacters()}
       </div>
     );
   }
